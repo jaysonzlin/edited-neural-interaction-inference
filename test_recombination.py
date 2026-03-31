946723f94481d5858caa6f11fd760fce04a4f374
Jayson Lin <jaysonlin@Jaysons-Mac-mini-5.local>
2026-03-31 11:19:52 -0400

    fix tensor error

--- test_recombination.py
+++ test_recombination.py
@@ -71,13 +71,13 @@ def main():
 
     # 4. Grab 1 batch of Springs and 1 batch of Charges
     (feat_s, edges_s), (rel_rec_s, rel_send_s), _ = dataset_s[0]
-    feat_s = torch.from_numpy(feat_s).unsqueeze(0).to(dev).float()
+    feat_s = torch.as_tensor(feat_s).unsqueeze(0).to(dev).float()
     
     (feat_c, edges_c), _, _ = dataset_c[0]
-    feat_c = torch.from_numpy(feat_c).unsqueeze(0).to(dev).float()
+    feat_c = torch.as_tensor(feat_c).unsqueeze(0).to(dev).float()
 
-    rel_rec = torch.from_numpy(dataset_s.rel_rec).to(dev).float()
-    rel_send = torch.from_numpy(dataset_s.rel_send).to(dev).float()
+    rel_rec = torch.as_tensor(dataset_s.rel_rec).to(dev).float()
+    rel_send = torch.as_tensor(dataset_s.rel_send).to(dev).float()
 
     # 5. Extract the Latent Blueprints!
     print("Encoding Blueprints...")
